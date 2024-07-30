5c16fc79f50803041ff9965dbf1692a1cc08fe62
Barahalikar Siddharth <barahalikar.siddharth@gmail.com>
2024-07-30 23:14:34 +0530

    ok

--- src/main/java/com/kodekloud/hello_demo/HelloDemoApplication.java
+++ src/main/java/com/kodekloud/hello_demo/HelloDemoApplication.java
@@ -1,15 +1,13 @@
 package com.kodekloud.hello_demo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController; 
+@SpringBootApplication
+public class HelloWorldApplication {
 
-@RestController
-public class HelloDemoApplication {
-
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello KodeKloud Users!";
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldApplication.class, args);
     }
 
 }
---
+++ src/main/java/com/kodekloud/hello_demo/controller/HelloDemoController.java
@@ -0,0 +1,16 @@
+package com.kodekloud.hello_demo;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController; 
+
+@RestController
+
+public class HelloDemoApplication {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello KodeKloud Users!";
+    }
+
+}
